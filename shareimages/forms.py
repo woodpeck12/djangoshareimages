4c1321bab8b1dc341014f8055f5f259fb42ed5e5
John <woodpeck1@gmail.com>
2018-11-13 18:11:44 +1100

    13-11-2018 still progress

--- shareimages/forms.py
+++ shareimages/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from .models import ShareIamge
-from urllb import request
+from urllib import request
 from django.core.files.base import ContentFile
-from django.utils.text import slugfy
+from django.utils.text import slugify
 
 class ShareImageCreateForm(forms.Model):
 	class Meta:
@@ -19,10 +19,10 @@ class ShareImageCreateForm(forms.Model):
 			raise forms.ValidationError('you are trying to load incorrect format of image. image must be joeg or jpg')
 		return url
 
-	def save(self,*args, **kwarg):
+	def save(self,commit=True,*args, **kwarg):
 		saved_img = super().save(commit=False)
 		img_url = self.cleaned_data['url']
-		img_name = '{}.{}'.format(slugfy(saved_img),img_url.rsplit('.',1)[1].lower())
+		img_name = '{}.{}'.format(slugify(saved_img),img_url.rsplit('.',1)[1].lower())
 
 
 		response= request.urlopen(img_url)
--- shareimages/views.py
+++ shareimages/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ShareImageCreateForm
+
+
+@login_required
+def shareimage_create(request):
+    if request.method == 'POST':
+        form = ShareImageCreateForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            received_date = form.save(commit=False)
+
+            received_date.user = request.User
+            received_date.save()
+            messages.success(request,'data is added')
+
